7efac8ffa4253b044ac5735bceb8f4a79a391287
Felipe <felipemunozramirez.fm@gmail.com>
2026-03-19 10:50:39 -0300

    Ahora si hay cuentas pares 3

--- src/LabEda/Main.java
+++ src/LabEda/Main.java
@@ -3,6 +3,7 @@ package LabEda;
 import edu.princeton.cs.algs4.Out;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StopwatchCPU;
+import edu.princeton.cs.algs4.DoublingRatio;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -28,7 +29,7 @@ public class Main {
             Out out = new Out("datos_ "+n+".csv");
             for(int i=0; i<100; i++){
                List<Integer> lista = generarLista(n);
-               int k = StdRandom.uniform(1,n);
+               int k = StdRandom.uniformInt(1, n);
                List<Integer> copia1 = new ArrayList<>(lista);
                 List<Integer> copia2 = new ArrayList<>(lista);
                 List<Integer> copia3 = new ArrayList<>(lista);
@@ -42,14 +43,14 @@ public class Main {
                 double tiempo2 = t2.elapsedTime();
 
                 StopwatchCPU t3 = new StopwatchCPU();
-                Result.cuentaPares2(n, copia2, k);
+                Result.cuentaPares3(n, copia3, k);
                 double tiempo3 = t3.elapsedTime();
 
                 out.println(tiempo1 + "," + tiempo2+ "," + tiempo3);
             }
             out.close();
         }
-        System.out.println("Experimento Finalizado!");
+        System.out.println("Experimento Finalizado");
     }
 
     }
